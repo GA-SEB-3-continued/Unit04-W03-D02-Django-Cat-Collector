d07cb85a83099dfda12eda104c5db73c2397cea3
Omar <omaraakamal@gmail.com>
2025-04-28 20:29:16 +0300

    cat-collector update

--- main_app/views.py
+++ main_app/views.py
@@ -11,3 +11,14 @@ def home(request):
 
 def about(request):
     return render(request,"about.html")
+
+def donate(request):
+    return render(request,"donate.html")
+
+
+# Create a donation page
+# 1. create the html in the templates file this should contain 1 button <button>donate<button>
+# 2. create a views function that should render the donation.html
+# 3. add your views to the urls.py file
+
+# 4. BONUS: use the base.html as a partial and display all the content in there
